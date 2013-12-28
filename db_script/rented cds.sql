10a84f297a74990feaf97f5280f6cb31bab0307e
Adonis <adonis.maynard@gmail.com>
2013-12-28 18:32:49 +0800

    [update]

--- db_script/rented cds.sql
+++ db_script/rented cds.sql
@@ -1,4 +1,4 @@
 ﻿/*Display the rented CDs.*/
-SELECT (tblcustomer.fname,  tblcustomer.mname, tblcustomer.lname) as rentedbyname, tblcdetail.movietitle as rented_cds
+SELECT (tblcustomer.fname,  tblcustomer.mname, tblcustomer.lname) as rented_by, tblcdetail.movietitle as rented_cds
 FROM tblcdonrent, tbltransaction, tblcdetail,tblcustomer
 WHERE tblcdonrent.cid=tblcustomer.cid and tblcustomer.cid=tbltransaction.cid and tblcdonrent.cdcode=tbltransaction.cdcode and tbltransaction.cdcode=tblcdetail.cdcode and tbltransaction.services='rent'
