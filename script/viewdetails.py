f0066101ca0e4fdbcafe9c40961a7e5d8fd1f64a
Adonis <adonis.maynard@gmail.com>
2013-12-29 20:23:30 +0800

    [color] change color and style

--- script/viewdetails.py
+++ script/viewdetails.py
@@ -23,22 +23,35 @@ def getmovieinfo(cdcode):
 def index(req, cdcode):
     cdcode = cgi.escape(cdcode)
     header = """
-	<!DOCTYPE html>
-	<html>
-	<head>
-	<title>Movies Database Example</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Bootstrap -->
-	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
-	<link href="bootstrap/css/theme.css" rel="stylesheet">
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-	    <![endif]-->
-	    """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+    content="width=device-width,
+    initial-scale=1.0"><meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+    <title>Using Bootstrap Template</title>
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/css/theme.css" rel="stylesheet">
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script
+    src="../../docs-assets/js/ie8-responsive-file-warning.js">
+    </script><![endif]-->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5
+    elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="bootstrap/js/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js">
+    </script>
+    <![endif]-->
+    """
     bodybegin = """
        </head>
        <body>
@@ -58,10 +71,10 @@ def index(req, cdcode):
     """
     movie = getmovieinfo(cdcode)
     moviecontainer = '<div class="container"> '
-    moviecontainer += ' <div class="starter-template"> '
+    moviecontainer += ' <div class="navar-header"> <div class="alert alert-info">'
     moviecontainer +=  ' <h1>Title: '+movie[1]+'</h1> '
-    moviecontainer +=   '<p class="lead">Genre:' + movie[2] + ' <br>'
-    moviecontainer +=   '<p class="lead">Availability:' + str(movie[3]) + ' <br>'
+    moviecontainer +=   '<p class="panel-body">Genre:' + movie[2] + ' <br>'
+    moviecontainer +=   '<p class="panel-body">Availability:' + str(movie[3]) + ' <br></div>'
     moviecontainer += '<br /> <a href="http://pythonista.learning.edu/~maynard/index.py"'
     moviecontainer += 'class="btn btn-success btn-sm active">Main Page</a></p></div></div>'
 
