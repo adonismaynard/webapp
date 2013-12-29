d3520755f1ba0b88fc1423d7e93d455995631ccd
Adonis <adonis.maynard@gmail.com>
2013-12-29 17:05:22 +0800

    [reposition] sequence of select attrib

--- script/index.py
+++ script/index.py
@@ -1,18 +1,20 @@
 import psycopg2
+
 def cdonrent():
     constr = """
         dbname='maynarddb'
         user='maynard'
         password='maynard123'
         host='pythonista.learning.edu'
-        """
+    """
     conn = psycopg2.connect(constr)
     curr = conn.cursor()
     curr.execute("""
-                 SELECT (tblcustomer.fname,
+                 SELECT tblcdetail.cdcode,
+                 tblcdetail.movietitle as rented_cds,
+                 tblcustomer.fname,
                  tblcustomer.mname,
-                 tblcustomer.lname) as rented_by,
-                 tblcdetail.movietitle as rented_cds
+                 tblcustomer.lname,
                  FROM tblcdonrent,
                  tbltransaction,
                  tblcdetail,
@@ -25,82 +27,81 @@ def cdonrent():
                  tbltransaction.petsa=tblcdonrent.petsa
                  """)
     rows = curr.fetchall()
+    return rows
+
 def index(req):
-     header ="""
-        <!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport"
-            content="width=device-width,
-            initial-scale=1.0">
-            <meta name="description" content="">
-            <meta name="author" content="">
-            <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
-            <title>Theme Template for Bootstrap</title>
-            <!-- Bootstrap core CSS -->
-            <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-            <!-- Bootstrap theme -->
-            <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
-            <!-- Custom styles for this template -->
-            <link href="bootstrap/css/theme.css" rel="stylesheet">
-            <!-- Just for debugging purposes. Don't actually copy this line! -->
-            <!--[if lt IE 9]><script
-            src="../../docs-assets/js/ie8-responsive-file-warning.js">
-            </script><![endif]-->
-            <!-- HTML5 shim and Respond.js IE8 support of HTML5
-            elements and media queries -->
-            <!--[if lt IE 9]>
-              <script
-                src="bootstrap/js/html5shiv.js"></script>
-              <script
-                src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js">
-                </script>
-            <![endif]-->
-            """
-bodybegin="""
-          </head>
-          <body>
+    header = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+    content="width=device-width,
+    initial-scale=1.0"><meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+    <title>Theme Template for Bootstrap</title>
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/css/theme.css" rel="stylesheet">
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script
+    src="../../docs-assets/js/ie8-responsive-file-warning.js">
+    </script><![endif]-->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5
+    elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="bootstrap/js/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js">
+    </script>
+    <![endif]-->
+    """
+    bodybegin="""
+        </head>
+        <body>
         <div class="container theme-showcase">
-          <!-- Main jumbotron for a primary
-          marketing message or call to action
-         -->
-      <div class="jumbotron">
+        <!-- Main jumbotron for a primary
+        marketing message or call to action
+        -->
+        <div class="jumbotron">
         <h1>Maynard Movies For Rent</h1>
         <p>Your offline movies are here. </p>
         <p>
         <a href="#" class="btn btn-primary btn-lg"
-         role="button">Learn more &raquo;</a></p>
-          </div>
-            <!-- Bootstrap core JavaScript
-            ================================================== -->
-            <!-- Placed at the end of the document so the pages load faster -->
-                <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-                <script src="../../dist/js/bootstrap.min.js"></script>
-                <script src="../../docs-assets/js/holder.js"></script>
-              </body>"""
+        role="button">Learn more &raquo;</a></p>
+        </div>"""
     bodyend = """
-           <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-             <script src="https://code.jquery.com/jquery.js"></script>
-           <!-- Include all compiled plugins (below), or include individual files as needed -->
-           <script src="js/bootstrap.min.js"></script>
-           </body>
-           </html>
-    """
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="../../dist/js/bootstrap.min.js"></script>
+        <script src="../../docs-assets/js/holder.js"></script>
+        </body>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include
+        individual files as needed-->
+        <script src="js/bootstrap.min.js"></script>
+        </body>
+        </html>
+        """
     panelbegin = """
-      <div class="panel panel-default">
-      <!-- Default panel contents -->
-      <div class="panel-heading">Listing</div>
-      <div class="panel-body">
-      """
+        <div class="panel panel-default">
+        <!-- Default panel contents -->
+        <div class="panel-heading">Rented CDs</div>
+        <div class="panel-body">
+        """
     tablebegin = """<table class="table table-hover table-condensed">"""
     tableend = "</table>"
     panelend = """
        </div>
       </div>
-    """
-
+      """
     movies = cdonrent()
     tablecontents = ""
     i = 1
@@ -109,19 +110,12 @@ bodybegin="""
             class_ = 'class="warning"'
         else:
             class_=""
-
         tablecontents += "<tr "+class_+">"
         tablecontents += '<td>'+movie[1]+"</td>"
-        tablecontents += "<td>"+movie[2]+"</td>"
-        tablecontents += "<td>"+str(movie[3])+"</td>"
-        tablecontents += '<td><a href="http://pythonista.learning.edu/~pythonista/viewdetails?movieid='+str(movie[0])+'" class="btn btn-info btn-sm active">Details</a></td>'
+        tablecontents += '<td>'+movie[2]+"</td>"
+        tablecontents += '<td>'+movie[3]+"</td>"
+        tablecontents += '<td>'+movie[4]+"</td>"
+        tablecontents += '<td><a href="http://pythonista.learning.edu/~maynard/viewdetails?movieid='+str(movie[0])+'" class="btn btn-info btn-sm active">Details</a></td>'
         tablecontents += "</tr>"
         i = i + 1
-
-
-
-
-
-
-
     return header + bodybegin + panelbegin + tablebegin + tablecontents + tableend + panelend + bodyend
