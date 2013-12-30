71b788cb4460ec57973f9b531de3c5abd85ea731
Adonis <adonis.maynard@gmail.com>
2013-12-30 11:05:47 +0800

    [sql] update returned query

--- db_script/returned cds.sql
+++ db_script/returned cds.sql
@@ -1,4 +1,4 @@
 ﻿/*Display the returned CDs.*/
-SELECT tblcdetail.movietitle as returned_cds, tblcustomer.fname,  tblcustomer.mname, tblcustomer.lname
+SELECT tblcdetail.movietitle as returned_cds, tblcustomer.fname,  tblcustomer.mname, tblcustomer.lname,tbltransaction.petsa as Date_Returned
 FROM tblcdreturned, tbltransaction, tblcdetail, tblcustomer
 WHERE tblcdreturned.cid=tblcustomer.cid and tblcustomer.cid=tbltransaction.cid and tblcdreturned.cdcode=tbltransaction.cdcode and tbltransaction.cdcode=tblcdetail.cdcode and tbltransaction.services='returned'
